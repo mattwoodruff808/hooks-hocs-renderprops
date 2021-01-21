e92605d4dc4e5d63eca5728fad7d209a4161fd60
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-21 11:41:41 -0700

    Coded along during lecture!

--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,44 +1,78 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react';
 
-class Hooks extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            username: '',
-            count: 0
-        }
-    }
+const Hooks = () => {
+    const [username, setUsername] = useState('');
+    const [count, setCount] = useState(0);
 
-    componentDidMount(){
-        this.setState({ username: 'Matt' })
-    }
+    useEffect(() => {
+        setUsername('Matt')
 
-    componentDidUpdate(){
+        // axios.get('/api/user')
+        //     .then(res => setUsername(res.data))
+        //     .catch(err => console.log(err))
+    }, [])
+
+    useEffect(() => {
         console.log('Count changed')
-    }
+    }, [count])
 
-    handleInput = (val) => {
-        this.setState({ username: val })
-    }
-
-    incrementCount = () => {
-        this.setState({ count: this.state.count + 1 })
-    }
-
-    decrementCount = () => {
-        this.setState({ count: this.state.count - 1 })
-    }
-
-    render(){
-        return (
-            <main>
-                <h1>Hello, {this.state.username}, your count is {this.state.count}</h1>
-                <input value={this.state.username} onChange={e => this.handleInput(e.target.value)}/>
-                <button onClick={this.decrementCount}>Count Down</button>
-                <button onClick={this.incrementCount}>Count Up</button>
-            </main>
-        )
-    }
+    return (
+        <main>
+            <h1>Hello, {username}, your count is {count}</h1>
+            <input value={username} onChange={e => setUsername(e.target.value)}/>
+            <button onClick={() => setCount(count - 1)}>Count Down</button>
+            <button onClick={() => setCount(count + 1)}>Count Up</button>
+        </main>
+    )
 }
 
 export default Hooks;
+
+
+
+
+
+// import React, { Component } from 'react'
+
+// class Hooks extends Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             username: '',
+//             count: 0
+//         }
+//     }
+
+//     componentDidMount(){
+//         this.setState({ username: 'Matt' })
+//     }
+
+//     componentDidUpdate(){
+//         console.log('Count changed')
+//     }
+
+//     handleInput = (val) => {
+//         this.setState({ username: val })
+//     }
+
+//     incrementCount = () => {
+//         this.setState({ count: this.state.count + 1 })
+//     }
+
+//     decrementCount = () => {
+//         this.setState({ count: this.state.count - 1 })
+//     }
+
+//     render(){
+//         return (
+            // <main>
+            //     <h1>Hello, {this.state.username}, your count is {this.state.count}</h1>
+            //     <input value={this.state.username} onChange={e => this.handleInput(e.target.value)}/>
+            //     <button onClick={this.decrementCount}>Count Down</button>
+            //     <button onClick={this.incrementCount}>Count Up</button>
+            // </main>
+//         )
+//     }
+// }
+
+// export default Hooks;
